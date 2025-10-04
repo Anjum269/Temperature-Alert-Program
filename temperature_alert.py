05142593b5d71d36f264ba5b29d47d393a6af955
Anjum <anjumkousar079@gmail.com>
2025-10-04 10:48:45 +0530

    Added Fahrenheit conversion feature

---
+++ temperature_alert.py
@@ -0,0 +1,16 @@
+ # temperature_alert.py
+
+temp_c = float(input("Enter temperature in °C: "))
+
+if temp_c < 20:
+    status = "Cold"
+elif 20 <= temp_c <= 30:
+    status = "Normal"
+else:
+    status = "Hot"
+
+temp_f = (temp_c * 9/5) + 32
+
+print(f"Temperature: {temp_c}°C")
+print(f"Status: {status}")
+print(f"Temperature in Fahrenheit: {temp_f}°F")
